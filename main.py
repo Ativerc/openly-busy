db1b6462578ef6eb1494fc17d38904585013cb73
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2021-09-24 12:44:10 +0530

    MQTT added. Could improve it though. Will add a config/constants file next.

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import subprocess
 import paho.mqtt.client as mqtt
 #from constants import SERVER, PORT, TIMEOUT
 
-statuses = ["NOT_BUSY", "BUSY"]
+statuses = ["NOT_BUSY", "BUSY", "APP_EXIT"]
 
-client = mqtt.Client()
+mqttclient = mqtt.Client()
 
-def mqtt_connect():
-    client.connect(SERVER, port=PORT, keepalive=TIMEOUT)
 
 def mqtt_send(current_status):
-    pass
+    mqttclient.connect("localhost", port=1883, keepalive=60)
+    mqttclient.publish("openbusylight/statuscode", payload=current_status, retain=True)
+
+def main_notifier(listt):
+    from_function = listt[0]
+    current_status = listt[1]
+    mqtt_send(current_status)
+    print(f"{from_function}: {current_status}")
 
 def cold_run_status_check():
     command = ["cat", "/proc/asound/card0/pcm0c/sub0/status"]
@@ -20,15 +25,11 @@ def cold_run_status_check():
     try:
         output = subprocess.check_output(('grep', 'RUNNING'), stdin=command_run.stdout)
         current_status = statuses[1] # Busy
-        print(f"Cold Run: {current_status}")
+        main_notifier(["Cold Run", current_status])
     except subprocess.CalledProcessError:
         current_status = statuses[0] # Not Busy
-        print(f"Cold Run: 2 {current_status}")
+        main_notifier(["Cold Run", current_status])
     return current_status
-
-def notifier_function():
-    pass
-
 
 def inotify_check():
     i = inotify.adapters.Inotify()
@@ -38,10 +39,10 @@ def inotify_check():
         #print(f"PATH=[{path}] EVENT_TYPES={type_names}")
         if type_names == ['IN_OPEN']:
             current_status = statuses[1]
-            print(f"{path} : {current_status}")
+            main_notifier([path, current_status])
         elif type_names == ['IN_CLOSE_WRITE']:
             current_status = statuses[0]
-            print(f"{path} : {current_status}")
+            main_notifier([path, current_status])
     
         # type_names 'IN_OPEN' 'IN_CLOSE_WRITE'
 
@@ -54,6 +55,7 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        mqtt_send(statuses[2])
         print("CtrlC Interrupt. Exiting...")
 
 
